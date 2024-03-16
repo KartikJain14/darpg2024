88045cc1806ec96735c191c352a97c5a3587f726
Kartik Jain <70012621+KartikJain14@users.noreply.github.com>
2024-03-16 16:35:30 +0530

    Update translate.py
    Remove small bug

--- vox_bridge/utils/translate.py
+++ vox_bridge/utils/translate.py
@@ -1,15 +1,16 @@
 import subprocess
+import os
 
 def translate_text(hi_text, en_text):
     try:
         with open(hi_text, "r", encoding="utf-8") as file:
             text = file.read()
-        
-        # Construct command as a list to avoid using the shell
-        command = ["argos-translate", "--from", "hi", "--to", "en", text]
-        
-        # Execute command without shell=True
-        result = subprocess.run(command, capture_output=True, text=True, check=True)
+
+        # Adjust the command to call the argos-translate without passing text directly as an argument
+        command = ["argos-translate", text , "--from-lang", "hi", "--to-lang", "en"]
+
+        # Execute the command and pass the text via stdin
+        result = subprocess.run(command, input=text, capture_output=True, text=True, check=True)
 
         translated_text = result.stdout.strip()
 
@@ -22,4 +23,5 @@ def translate_text(hi_text, en_text):
     except Exception as e:
         print(f"An error occurred: {e}")
         return False
+
     return True
