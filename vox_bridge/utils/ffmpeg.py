3ac5a09709f9cf455010c131a2766008f31c8462
Kartik Jain <70012621+KartikJain14@users.noreply.github.com>
2024-03-18 19:47:52 +0530

    Update ffmpeg.py
    No more logs.

--- vox_bridge/utils/ffmpeg.py
+++ vox_bridge/utils/ffmpeg.py
@@ -3,7 +3,7 @@ def convert_audio(input_file, output_file):
     (
         ffmpeg
         .input(input_file)
-        .output(output_file, acodec='pcm_s16le', ac=1, ar='16000')
+        .output(output_file, acodec='pcm_s16le', ac=1, ar='16000', loglevel="quiet")
         .overwrite_output()
         .run()
     )
